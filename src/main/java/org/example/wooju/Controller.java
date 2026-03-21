c6efda9e054c8cd99fa54b929206a963e11a758f
01 <parkjua78@gmail.com>
2026-03-21 14:59:58 +0900

    Controller change

--- src/main/java/org/example/wooju/Controller.java
+++ src/main/java/org/example/wooju/Controller.java
@@ -1,9 +1,11 @@
+package org.example.wooju;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class Controller {
-    @GetMapping("/")
+    @GetMapping("/test")
     public String hello() {
         return "Hello, CD Test v1"; // 나중에 v2로 바꿔서 테스트할 부분입니다.
     }
